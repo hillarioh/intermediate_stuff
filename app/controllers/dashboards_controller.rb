852fa97f806169d62702a286025f3957bd2ac158
hillarioh <okerioh@gmail.com>
2022-04-12 12:39:38 +0300

    add partial and user and shout show page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,6 @@ class DashboardsController < ApplicationController
     def show
         @shout = Shout.new
         @shouts = current_user.shouts
+        @user = current_user
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
     root to: "dashboards#show"
   end
 
-# constraints Clearance::Constraints::SignedOut.new do
-#     root to: 'homes#show'
-#   end
-
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
-  resource :shouts, only: [:create]
+  resources :shouts, only: [:create, :show]
 
-  resources :users, only: [:create] do
+  resources :users, only: [:create, :show] do
     resource :password,
       controller: "clearance/passwords",
       only: [:edit, :update]
