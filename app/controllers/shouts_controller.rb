5756644848c97865271e348122f501d86c7d218d
hillarioh <okerioh@gmail.com>
2022-04-13 05:30:20 +0300

    refactor application to be porlymorphic

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -8,8 +8,17 @@ class ShoutsController < ApplicationController
         @shout = Shout.find(params[:id])
    end
 
+    private
     def shout_params
-        params.require(:shout).permit(:body)
+        {content: content_from_params}
+    end
+
+    def content_from_params
+        TextShout.new(content_params)
+    end
+
+    def content_params
+        params.require(:shout).require(:content).permit(:body)
     end
 
     def redirect_options_for(shout)
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,9 +1,7 @@
 class Shout < ApplicationRecord
   belongs_to :user
-  validates :body, presence: true, length: {in: 10..144}
+  belongs_to :content, polymorphic: true
   validates :user, presence: true
-
   delegate :username, to: :user
-
   default_scope { order(created_at: :desc)}
 end
